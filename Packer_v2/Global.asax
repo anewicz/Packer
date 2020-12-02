f3c002ee371e93db47d40b7942374ca0a9d27ad1
anewicz <ane.michalewicz@gmail.com>
2020-12-02 00:39:34 -0300

    Tela com Registro/Login

---
+++ Packer_v2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Packer_v2.MvcApplication" Language="C#" %>
---
+++ Packer_v2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Packer_v2.Startup))]
+namespace Packer_v2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
